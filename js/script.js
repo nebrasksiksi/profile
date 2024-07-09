980ee4df1a812a47d3f70babd1fd1b46dc1091a6
nebrasksiksi <ksiksinebras@gmail.com>
2024-07-09 16:27:59 +0100

    initialisation

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*================typing animation===============*/ 
+var typed = new Typed(".typing",{
+    strings:["","IT engineering student"]
+    //typeSpeed:100,
+    //BackSpeed:60,
+    //loop:true
+
+})
